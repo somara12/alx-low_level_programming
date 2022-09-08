562630ff1c822f906db1cc6ada1ea681a1505bcc
Chidimma Ezeadireje <xtilence@gmail.com>
2022-09-08 07:08:28 -0700

    My Level C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/* Printing */
+/**
+  * main - Printin with f
+  *
+  * Return: Always 0 (when it is a Success)
+  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
