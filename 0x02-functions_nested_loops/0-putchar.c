20465f98e589898d8e3d25c6385bc340d4faf4e1
Chidimma Ezeadireje <xtilence@gmail.com>
2022-09-13 04:45:27 -0700

    My first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c = "_putchar";
+
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
