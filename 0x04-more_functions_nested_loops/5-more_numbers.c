71d664316246a0a78117e62e7b8d39f69fce73bb
Chidimma Ezeadireje <xtilence@gmail.com>
2022-09-15 06:20:48 -0700

    Nested

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,15 @@
 
 void more_numbers(void);
 {
-	int a, b;
+	int i = 0, j = 0;
 
-	for (a = 0; a < 10; a++)
+	for (i = 0; i < 10; i++)
 	{
-		for (b = 0; b < 15; b++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (b / 10 != 0)
-			{
-				_putchar(b / 10 + '0');
-			}
-			_putchar(b % 10 + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
