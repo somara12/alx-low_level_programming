fd1af4c8f0b60194a7c4bb62812f84a90cacb5cb
Chidimma Ezeadireje <xtilence@gmail.com>
2022-09-30 08:59:55 -0700

    Doing hard things

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_memset - The _memset() function fills
+* the byte
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+
+}
